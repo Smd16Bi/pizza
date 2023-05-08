2bd9b5d036540cdf4cfd28c31930e406361e0b8f
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-05-08 06:45:20 +0300

    video three part one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react'
 import "./scss/app.scss"
-import Header from './components/header/Header'
 
-
+import { Header, Categories } from './components/index'
 
 const App = () => {
   return (
     <div className="wrapper">
-        <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              onClick={(name)=>console.log(name)}
+              items={["All", "Meat", "Vegetarian", "Grill", "Spicy", "Closed"]}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// import Button from "./button/Button";
+// import Categories from "./categories/Categories";
+// import Header from "./header/Header";
+// export { Button, Categories, Header }
+
+
+export { default as Button } from "./button/Button"
+export { default as Categories } from "./categories/Categories"
+export { default as Header } from "./header/Header"
