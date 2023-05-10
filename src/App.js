eec1fbb684ec7c1ff706259df3d28b971be7fcfa
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-05-10 06:56:23 +0300

    video five part one

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import "./scss/app.scss"
-import { Header } from './components/index'
-
-// import { Home } from "./pages/Home"
-// import { Cart } from "./pages/Cart"
-import { Route, Routes } from 'react-router-dom'
-import { Cart, Home } from './pages'
+import React from 'react';
+import "./scss/app.scss";
+import { Header } from './components/index';
+import { Route, Routes } from 'react-router-dom';
+import { Cart, Home } from './pages';
 
 const App = () => {
+  const [pizzas, setPizzas] = React.useState([])
+  React.useEffect(() => {
+    async function getData() {
+      const response = await fetch("/db.json");
+      const jsonData = await response.json();
+      setPizzas(jsonData.pizzas);
+    }
+    getData()
+  }, []);
+
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home items={pizzas}  />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
         </Routes>
       </div>
