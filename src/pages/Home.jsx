9015ddb5ea26854dd7b1705c73db4c2d1e9439b4
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-05-15 06:11:32 +0300

    video six part two

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = ({ items = [] }) => {
             </div>
             <h2 className="content__title">All</h2>
             <div className="content__items">
-                {items.map(obj => {
+                {items[0].map(obj => {
                     return <PizzaBlock key={obj.id} {...obj} />
                 })}
             </div>
