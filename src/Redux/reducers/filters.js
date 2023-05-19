c37a50a975912e2108d425c0b1b138abf988084f
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-05-19 05:50:21 +0300

    video-eight part three

--- src/Redux/reducers/filters.js
+++ src/Redux/reducers/filters.js
@@ -1,24 +1,26 @@
 const initialState = {
-    category: 0,
-    sortBy: "Popularity",
-};
-const filters = (state = initialState, action) => {
-
-    if (action.type === "SET_SORT_BY") {
-        return {
-            ...state,
-            sortBy:action.payload
-        }
+    category: null,
+    sortBy: {
+      type: 'popular',
+      order: 'desc',
+    },
+  };
+  
+  const filters = (state = initialState, action) => {
+    if (action.type === 'SET_SORT_BY') {
+      return {
+        ...state,
+        sortBy: action.payload,
+      };
     }
-    if (action.type === "SET_CATEGORY") {
-        return {
-            ...state,
-            category:action.payload
-        }
+    if (action.type === 'SET_CATEGORY') {
+      return {
+        ...state,
+        category: action.payload,
+      };
     }
-
-    return state
-
-}
-
-export default filters
+    return state;
+  };
+  
+  export default filters;
+  
