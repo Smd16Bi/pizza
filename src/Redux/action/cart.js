ea2488f65eadd81c854eaa52447c6ee1da386ee9
Anton Vasylchenko <106306442+maxelectriclabs@users.noreply.github.com>
2023-05-23 06:40:16 +0300

    finish

--- src/Redux/action/cart.js
+++ src/Redux/action/cart.js
@@ -5,3 +5,17 @@ export const addPizzaToCart = (pizzaObj) => ({
 export const clearCart = () => ({
     type: "CLEAR_CART"
 })
+export const removeCartItem = (id) => ({
+    type: "REMOVE_CART_ITEM",
+    payload:id
+})
+export const plusItem = (id) => ({
+    type: "PLUS_CART_ITEM",
+    payload:id
+})
+export const minusItem = (id) => ({
+    type: "MINUS_CART_ITEM",
+    payload:id
+})
+
+
